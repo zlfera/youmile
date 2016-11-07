00d01b48770011550a8001070b385f4c0d89b227
zlfera <zlfera@gmail.com>
2016-11-07 16:58:11 +0800

    40

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
     #expires_in 12.hours
     #fresh_when(Time.new.day)
-    etag {1}
     respond_to do |format|
       format.html
     end
