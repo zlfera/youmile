42255b717004e07b344086a353c057dca77e8adc
zlfera <zlfera@gmail.com>
2016-11-08 13:00:23 +0800

    41

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     #expires_in 12.hours
-    #fresh_when(etag: 'zeng ing fa', public: true)
+    fresh_when(etag: 'zeng ing fa', public: true)
     #respond_to do |format|
       #format.html
     #end
