e1b91fb1aede274212a5352765a3a62644ae6ee9
zlfera <zlfera@gmail.com>
2017-03-19 19:32:05 +0800

    68

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     # expires_in 12.hours
-    fresh_when(etag: Time.new.day, public: true)
+    #fresh_when(etag: Time.new.day, public: true)
     # system("rails first")
     # respond_to do |format|
     # format.html
