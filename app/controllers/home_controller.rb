d50ec655687fd4666a855951f1d98041f88b524e
zlfera <zlfera@gmail.com>
2018-12-24 14:04:20 +0000

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,19 +19,27 @@ class HomeController < ApplicationController
     #where("variety ='中晚籼稻' or variety = '早籼稻'")
     # @variety = params[:variety]
     td_data = params[:td_data]
+    search = params[:search]
     if td_data == nil
       @redis = Grain.where("latest_price != '0'").order(created_at: :desc)
     else
-      x, y = td_data.split(',')
-      if y == 'latest_price' || y == 'starting_price'
-        a=Grain.where("#{y} != '0'").order(created_at: :desc).minimum(y)
-        @redis = Grain.where("#{y} = '#{a}'")
-        #@redis = Grain.where("#{y} <= '#{x}'").where("latest_price != '0'").where("latest_price != '拍卖'").order(created_at: :desc)
-      else
-        if y == 'address'
-          x, _ = x.split('(')
+      if search == nil
+        x, y = td_data.split(',')
+        if y == 'latest_price' || y == 'starting_price'
+          a=Grain.where("#{y} != '0'").order(created_at: :desc).minimum(y)
+          @redis = Grain.where("#{y} = '#{a}'")
+          #@redis = Grain.where("#{y} <= '#{x}'").where("latest_price != '0'").where("latest_price != '拍卖'").order(created_at: :desc)
+        else
+          if y == 'address'
+            x, _ = x.split('(')
+          end
+          @redis = Grain.where("#{y} = '#{x}'").where("latest_price != '0'").order(created_at: :desc)
         end
-        @redis = Grain.where("#{y} = '#{x}'").where("latest_price != '0'").order(created_at: :desc)
+      else
+        a = Grain.order(created_at: :desc).all
+        @redis = a.reject do |i|
+          i['address'].include(search) == false
+        end
       end
     end
     #@redis = Redis.new(url: Rails.application.secrets.redis_url)
